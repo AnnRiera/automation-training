f9b23c5b85b850429ab486ac5f8e8781e0727b57
Ana Riera <annriera@outlook.com>
2023-06-17 13:54:04 -0400

    adding new tests for use case: buying process

--- cypress/e2e/userWorkflow.spec.js
+++ cypress/e2e/userWorkflow.spec.js
@@ -1,19 +1,40 @@
 /// <reference types="cypress" />
 
 const baseUrl = Cypress.env('BASE_URL');
+const pickedProductUrl = Cypress.env('PRODUCT_URL');
+const productId = 28;
+const productName = 'Pure Cotton V-Neck T-Shirt';
 
-describe('Home page', () => {
-    beforeEach('Reload page', () => {
-        cy.visit(baseUrl);
-    });
+describe('User flow', () => {
+    // beforeEach('Reload page', () => {
+    //     cy.visit(baseUrl);
+    // });
 
-    describe('Testing home page', () => {
-        it('should scroll to the middle of the page', () => {
+    describe('USE CASE: buying process', () => {
+        it('should view products', () => {
+            // should scroll to the middle of the page
+            cy.visit(baseUrl);
             cy.scrollTo('center');
+
+            // should pick Pure Cotton V-Neck T-Shirt
+            cy.get('.features_items .col-sm-4')
+                .then((product) => {
+                    // TODO: here should get the product price
+                    expect(product[21]).contain(productName);
+                    cy.wrap(product[21])
+                        .contains('View Product')
+                        .click();
+                    // check if is the same url
+                    cy.url()
+                        .should('eq', `${pickedProductUrl}/${productId}`);
+                    // check if it's the same product
+                    cy.get('[class="product-information"] h2')
+                        .should('contain',productName);
+                });
         });
 
-        it('should pick the Colour Blocked Shirt - Sky Blue and click View product button', () => {
+        // it('should add products to the cart', () => {
 
-        });
+        // });
     });
 });
