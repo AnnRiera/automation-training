7b5b9d7d42a18261a408fa959c3d03b9a7a71c6c
Ana Riera <AnnRiera@outlook.com>
2023-06-28 14:10:16 -0400

    readme and format

--- README.md
+++ README.md
@@ -1,23 +1,51 @@
-
 # Cypress automation training
 
-This project is a brief exercise, the idea is automatize
+This project is a brief exercise, the idea is automate the user's flow on a test [page](BASE_URL=https://automationexercise.com/) provided by the mentor.
 
 ## Configuration
 
-To start this project is necessary run the following command:
+This configuration runs okay on Windows and Ubuntu without any further configuration. To start this project is necessary to run the following command:
 
 ```
 npm install
 ```
 
-You also has three commands to run the project:
+You also need to have a `.env` file with the URL of the website.
+
+```
+BASE_URL=https://example.com/
+```
+
+## Commands
 
 ### Headless
 
-To run the porject with headless mode please run: 
+To run the porject with headless mode please run:
+
 ```
 npm run headless
 ```
 
-### 
+### Headed
+
+To run the porject with headed mode please run:
+
+```
+npm run headed
+```
+
+### Reports
+
+This command provides you a HTML auto-generated report:
+
+```
+npm run reports
+```
+
+### Format
+
+This project uses prettier and eslint to code format, so if you want to formatting your files:
+
+```
+npm run format
+```
--- constants.js
+++ constants.js
@@ -1,31 +1,35 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const quantity = 30;
-const subject = 'Test subject';
-const comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const subject = "Test subject";
+const comment =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 const userInfo = {
-    name: 'Jane Doe',
-    email: `jane.doe@${crypto.randomBytes(8).toString("hex")}.com`,
-    password: 'Abc123.',
-    dayOfBirth: '8',
-    monthOfBirth: '6',
-    yearOfBirth: '1995',
-    company: 'Example Company',
-    address: '11421 NW 39th ST Doral, FL 33178.',
-    country: 'United States',
-    state: 'FL',
-    city: 'Doral',
-    zipCode: '33178',
-    phoneNumber: '+1 305 400 87 71',
-    payment: {
-        cardName: 'Jane Doe',
-        cardNumber: '5422533266019495',
-        cvv: '748',
-        expirationMonth: '05',
-        expirationYear: '2030',
-    }
+  name: "Jane Doe",
+  email: `jane.doe@${crypto.randomBytes(8).toString("hex")}.com`,
+  password: "Abc123.",
+  dayOfBirth: "8",
+  monthOfBirth: "6",
+  yearOfBirth: "1995",
+  company: "Example Company",
+  address: "11421 NW 39th ST Doral, FL 33178.",
+  country: "United States",
+  state: "FL",
+  city: "Doral",
+  zipCode: "33178",
+  phoneNumber: "+1 305 400 87 71",
+  payment: {
+    cardName: "Jane Doe",
+    cardNumber: "5422533266019495",
+    cvv: "748",
+    expirationMonth: "05",
+    expirationYear: "2030",
+  },
 };
 
 module.exports = {
-    quantity, userInfo, comment, subject,
+  quantity,
+  userInfo,
+  comment,
+  subject,
 };
